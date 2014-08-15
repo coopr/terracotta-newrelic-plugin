b5eadc9e83b565a0a5ff77d3a1707d84c56a5cfb
jeffsegal <jeffrey.segal@gmail.com>
2014-08-15 14:18:48 -0400

    Fixing typo

--- src/main/scripts/stop-tc.sh
+++ src/main/scripts/stop-tc.sh
@@ -2,4 +2,4 @@
 
 TC_HOME=/opt/terracotta/bigmemory-max-4.1.3
 sh $TC_HOME/server/bin/stop-tc-server.sh
-sh $YC_HOME/tools/management-console/bin/stop-tmc.sh
+sh $TC_HOME/tools/management-console/bin/stop-tmc.sh
